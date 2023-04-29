29bce0f2162523b9402b30f396838382759d278d
donohoe14 <donohoe14@aol.com>
2023-04-29 19:51:14 +0100

    test3

--- 2chapter/main.tf
+++ 2chapter/main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "example" {
-  ami           = "ami-0aa2b7722dc1b5612"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [ aws_security_group.instance.id ]
+resource "aws_launch_configuration" "example" {
+  image_id            = "ami-0aa2b7722dc1b5612"
+  instance_type       = "t2.micro"
+  vpc_security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
 #!/bin/bash
@@ -15,11 +15,27 @@ echo "Hello, world" > index.html
 nohup busybox httpd -f -p ${var.server_port} &
 EOF
 
-  user_data_replace_on_change = true
-
   tags = {
     Name = "terraform-example"
   }
+}
+
+lifecycle {
+    create_before_destroy = true
+}
+
+resource "aws_autoscaling_group" "name" {
+  launch_configuration = aws_launch_configuration.example.name
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = name
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+
 }
 
 resource "aws_security_group" "instance" {
@@ -34,15 +50,17 @@ resource "aws_security_group" "instance" {
 }
 
 variable "server_port" {
-    description = "the [port the server will use for http requests]"
-    type = number
-    default = 8080  
+  description = "the [port the server will use for http requests]"
+  type        = number
+  default     = 8080
 }
 
 output "public_ip" {
-  value = aws_instance.example.public_ip
+  value       = aws_instance.example.public_ip
   description = "the public ip of the web server"
 }
+
+
 
 
 
