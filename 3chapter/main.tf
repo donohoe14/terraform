02bef7ceac9dcacdecbc99fd14e7ec7989446ae7
donohoe14 <donohoe14@aol.com>
2023-05-01 20:03:52 +0100

    outputs s3 and dynamodb names

--- 3chapter/main.tf
+++ 3chapter/main.tf
@@ -56,3 +56,14 @@ terraform {
   }
 
 }
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the s3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the dynamodb table"
+
+}
